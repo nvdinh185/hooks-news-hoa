9274b39094888c3d9a7ddf0c67dfe4aaac125194
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-16 09:22:00 +0700

    x

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,8 +1,7 @@
 function PostItem(prop) {
-    var urlPic = `images/${prop.img}`;
     return (
         <li>
-            <a href="#"><img src={urlPic} alt={prop.img} /></a>
+            <a href="#"><img src={'images/' + prop.img} alt={prop.img} /></a>
             <div className="khoiphai">
                 <h2><a href="#">{prop.title}</a></h2>
                 <p>{prop.content}</p>
